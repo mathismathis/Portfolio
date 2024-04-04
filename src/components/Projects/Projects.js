e919d9f067a92ed90b4ed094f89474a4f990f39c
mathismathis <mathismathis2000tt@gmail.com>
2024-04-04 20:50:14 +0530

    tes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,43 +1,275 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-
-
+import { Container } from "react-bootstrap";
 
 function Projects() {
   return (
     <Container fluid className="project-section">
-    
       <Container>
-        <h1 className="project-heading">
+
+
+
+      <h1 className="project-heading">
           My Recent <strong className="purple">Works </strong>
         </h1>
         <p style={{ color: "white" }}>
           Here are a few projects I've worked on recently.
         </p>
-        <div className="card mb-3">
+
+      <div className="card mb-3">
+          <div className="card-body text-start">
+
+          <h1 className="card-text"><strong> DataTerrain India Private Ltd. </strong></h1>
+          <hr />
      
-        <div className="card-body text-start">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+            <h5 className="card-title"><strong>Position:</strong> Software Developer</h5>
+            <h5 className="card-title"><strong>Duration:</strong> Since January 17, 2022, until now.</h5>
+            <p className="card-text"><strong>Address:</strong> 1A, 1st Floor, Prince Arcade, 22A (New), No.29, Cathedral Rd, Chennai, Tamil Nadu 600086</p>
+            
+            
+            
+            
+            
+            <p className="card-text"><strong>Website:</strong> <a href="https://dataterrain.com/" style={{ color: "#6c63ff" }}>https://dataterrain.com/</a></p>
+            <hr />
 
-          <br /> {/* Corrected self-closing tag */}
+            <p className="card-text">
 
-
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-
-        </div>
-      </div>
-      <div className="card">
-        <div className="card-body text-start">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-        </div>
         
-      </div>
+
+<h2 className="card-title "><strong>Project - 1 : </strong><strong className="purple"> Amazon FinTech Netherlands</strong> </h2>
+
+<hr />
+    <p className="card-text" style={{ backgroundColor: "#f2f2f2", padding: "10px", borderRadius: "5px" }}>
+      <strong>Project Overview:</strong> 
+      
+      The project involves close collaboration with Amazon's employees to streamline and manage tax filing data effectively. This collaboration is essential for ensuring compliance with local tax regulations, optimizing processes, and enhancing the accuracy and efficiency of tax-related operations. By working closely with Amazon's personnel
+      </p>
+    <hr />
+    
+    <strong>Responsibilities and Achievements:</strong>
+    <ul>
+        <li>Utilization of Apache Airflow for scheduling and automating various tasks related to tax data management.</li>
+        <li>Implementation of data ingestion and preprocessing pipelines using AWS EMR (Elastic MapReduce) to handle large volumes of tax data efficiently.</li>
+        <li>Creation of Lambda functions to maintain Glue tables and automate the provisioning and management of EMR clusters, ensuring smooth operation and scalability of the system.</li>
+        <li>Development of an SNS (Simple Notification Service) and SQS (Simple Queue Service) alert system for timely notification and handling of system events or anomalies, enhancing the system's monitoring capabilities.</li>
+        <li>Configuration of CloudWatch alerts to monitor system metrics, ensuring proactive detection and resolution of issues.</li>
+        <li>Creation of dashboards using AWS services like CloudWatch and QuickSight to provide stakeholders with insightful visualizations and real-time data monitoring, facilitating informed decision-making and system optimization.</li>
+      </ul>
+    </p>
+
+    <hr />
+    <p><strong>Used Skills:</strong> Python Apache Airflow, AWS EMR (Elastic MapReduce), AWS Lambda, AWS Glue, AWS SNS (Simple Notification Service) and SQS (Simple Queue Service), AWS CloudWatch, AWS QuickSight, AWS EC2 (Elastic Compute Cloud), AWS IAM (Identity and Access Management), Athena
+Amazon Redshift </p>
+    <hr />
+            
+            <p className="card-text">
+
+            <h2 className="card-title"><strong>Project - 2 : </strong><strong className="purple"> HR Application Chatbot Development</strong></h2>
+
+        <hr />
+        <p className="card-text" style={{ backgroundColor: "#f2f2f2", padding: "10px", borderRadius: "5px" }}>
+              <strong>Project Overview:</strong> 
+              The HR Application Chatbot Development project aimed to streamline employee inquiries regarding HR-related matters by implementing a conversational AI solution. The primary objective was to provide employees with quick and accurate responses to their queries, such as available leave balances, policies, and other HR-related information.
+            </p>
+            <hr />
+            
+              <strong>Responsibilities:</strong>
+           
+              <ul>
+                <li>Natural Language Understanding (NLU): Integrated advanced NLU capabilities to comprehend and interpret user queries accurately, ensuring precise responses.</li>
+                <li>Query Resolution: Developed algorithms and processes to extract relevant information from the HR database based on user queries, such as available leave balances, company policies, and procedures.</li>
+                <li>Conversational Interface: Designed a user-friendly conversational interface to facilitate seamless interactions between employees and the chatbot, promoting ease of use and accessibility.</li>
+                <li>Personalized Responses: Implemented personalized responses based on user profiles and historical data to enhance user experience and relevance.</li>
+                <li>Continuous Learning: Incorporated mechanisms for continuous learning and improvement, allowing the chatbot to adapt to evolving HR policies and employee preferences over time.</li>
+                <li>Integration: Integrated the chatbot with existing HR systems and databases to ensure real-time access to accurate information and data synchronization.</li>
+              </ul>
+
+              <hr />
+
+              <p>
+  <strong>Used Skills: </strong> 
+  Python,TensorFlow,Data Preprocessing and Labeling,Model Fine-tuning, Pandas,SQL,Web scraping
+
+</p>
+
+            </p>
+
+
+            <hr />
+            <p className="card-text">
+
+        
+
+            <h2 className="card-title"><strong>Project - 3 : </strong> <strong className="purple">Root Clinic Medical Client Application Development</strong></h2>
+       
+        <hr />
+        <p className="card-text" style={{ backgroundColor: "#f2f2f2", padding: "10px", borderRadius: "5px" }}>
+              <strong>Project Overview:</strong> 
+              I spearheaded the development of a comprehensive client application portal for Root Clinic, focusing on dynamic form creation, client chart management, client care planning, and appointment scheduling. The project involved close collaboration with medical professionals to ensure the application met the specific needs of the clinic and its patients.
+            </p>        <hr />
+            
+            <strong>Responsibilities and Achievements:</strong>
+            <ul>
+                <li>Dynamic Form Creation: Designed and implemented a system for creating dynamic forms tailored to the diverse needs of clients, enabling efficient data collection and management.</li>
+                <li>Client Chart Management: Developed features for maintaining detailed client charts, including medical history, treatment plans, progress notes, and other relevant information, ensuring comprehensive and accurate record-keeping.</li>
+                <li>Client Care Planning: Collaborated with healthcare providers to design and integrate client care planning functionalities, allowing for the creation, customization, and tracking of individualized care plans to optimize patient outcomes.</li>
+                <li>Appointment Scheduling: Implemented a robust appointment scheduling system, enabling clients to book appointments online, view availability, and receive automated reminders, streamlining the scheduling process for both clients and staff.</li>
+              </ul>
+
+              <p>
+              <strong>Used Skills:</strong> 
+
+ Python,Django Rest Framework,React.js,Bootstrap,Tailwind CSS,PostgreSQL  </p>
+
+            </p>
+          </div>
+        </div>
+ 
+
+     
+    
+        <div className="card mb-3">
+          <div className="card-body text-start">
+
+          <h1 className="card-text"><strong>Zinnov Management Consulting Pvt Ltd</strong> </h1>
+          <hr />
+          
+            <h5 className="card-title"><strong>Position:</strong> Python Developer</h5>
+            <h5 className="card-title"><strong>Duration:</strong> 17-Jan-2022 to 15-Mar-2023</h5>
+
+            <p className="card-text"><strong>Address:</strong> Avarampalayam, Illango Nagar, Coimbatore, Tamil Nadu 641006</p>
+            <p className="card-text"><strong>Website:</strong> <a href="https://draup.com/" style={{ color: "#6c63ff" }}>https://draup.com/</a></p>
+            <hr />
+            <p className="card-text">
+
+            <h2 className="card-title"><strong>Project - 1 : </strong>
+          <strong className="purple">DraupProfile Big Data Management</strong>
+        </h2>
+
+        <hr />
+            <p className="card-text" style={{ backgroundColor: "#f2f2f2", padding: "10px", borderRadius: "5px" }}>
+              <strong>Project Overview:</strong> 
+              The DraupProfile project involved managing big data utilizing Python and Pyspark. This project focused on analyzing large datasets containing millions of rows, performing various data transformations, and delivering actionable insights to stakeholders. The responsibilities included creating dataframes, performing joins, converting SQL queries to Pyspark, and developing codes in a cloud-based environment.
+            </p>
+            <hr />
+            
+              <strong>Responsibilities:</strong>
+              <ul>
+                <li>Creating dataframes in Pyspark for analyzing datasets containing millions of rows.</li>
+                <li>Performing multiple joins of dataframes using Pyspark to deliver the required dataset to stakeholders.</li>
+                <li>Converting SQL queries to Pyspark queries for accessing tables in Postgres SQL DB.</li>
+                <li>Developing Pyspark and Pandas codes using AWS EMR environment.</li>
+                <li>Collecting required data from several websites, transforming it into the required format using Python packages such as Selenium, BeautifulSoup, Pandas.</li>
+                <li>Automating a large number of manual processes with the help of Selenium.</li>
+              </ul>
+            </p>
+
+            <hr />
+
+            <p>
+            <strong>Used Skills: </strong> 
+
+                  Python,
+          PySpark,
+          Pandas,
+          EMR,
+          EC2,
+          SQL,
+          Tableau,
+          Excel automation,
+          Selenium,
+          Data Preprocessing.
+          </p>  
+
+
+
+            <hr />
+            <p className="card-text">
+
+            <h2 className="card-title"><strong>Project 2 : </strong>
+         <strong className="purple"> AMS Data collection Project</strong>
+        </h2>
+
+        <hr />
+            <p className="card-text" style={{ backgroundColor: "#f2f2f2", padding: "10px", borderRadius: "5px" }}>
+              <strong>Project Overview:</strong> 
+              Streamlined job posting analysis by automating data collection from hh.ru and LinkedIn, with a specific focus on gathering data related to the companies requested by the client. Conducted large-scale analysis using PySpark and provided actionable insights tailored to stakeholders' needs and interests.
+            </p>
+            <hr />
+            
+            <strong>Responsibilities and Achievements:</strong>
+              <ul>
+                <li>Designed and implemented an automated data collection pipeline using Selenium to extract job postings from hh.ru and LinkedIn, ensuring efficiency and accuracy in data retrieval.</li>
+                <li>Developed data preprocessing routines to clean and format collected data, ensuring suitability for analysis.</li>
+                <li>Utilized PySpark for distributed data processing, enabling scalable analysis of large volumes of job posting data.</li>
+                <li>Conducted exploratory data analysis (EDA) to gain insights into distribution and characteristics of job postings.</li>
+                <li>Applied statistical methods and machine learning algorithms to uncover patterns and trends in job postings by listed companies.</li>
+                <li>Collaborated with team members to interpret analysis results and generate actionable insights for stakeholders.</li>
+                <li>Documented project workflow, methodologies, and findings in a clear and concise manner.</li>
+              </ul>
+
+
+              <p>
+            <strong>Used Skills: </strong> 
+            Selenium,
+                  Python,
+          PySpark,
+          Pandas,
+          SQL,
+          Data Preprocessing.
+          </p>  
+
+
+            </p>
+          </div>
+        </div>
+
+
+       
+
+
+       
+        <div className="card mb-3">
+          <div className="card-body text-start">
+
+          <h1 className="card-text"><strong>Clovion Tech Solutions Pvt. Ltd</strong> </h1>
+          <hr />
+          
+            <h5 className="card-title"><strong>Position:</strong> Python Developer</h5>
+      
+            <p className="card-text"><strong>Duration:</strong> July 13, 2021, to January 15, 2022</p>
+            <p className="card-text"><strong>Location:</strong> 1st floor, AJR Complex, Azhagiyamandapam, Jn, Tamil Nadu 629167</p>
+            <p className="card-text"><strong>Website:</strong> <a href="https://www.clovion.com" style={{ color: "#6c63ff" }}>Clovion Tech Solutions Pvt. Ltd</a></p>
+            <hr />
+
+            <h2 className="card-title"><strong >Project - 1 : <strong className="purple">Bright Future Academy Chat Application</strong> </strong></h2>
+            <hr />
+
+            <p className="card-text" style={{ backgroundColor: "#f2f2f2", padding: "10px", borderRadius: "5px" }}>
+              <strong>Project Overview:</strong> 
+              During my tenure at Clovion Tech Solutions Pvt. Ltd, I actively contributed to the development of the Bright Future Academy Chat Application, utilizing Python Django for an educational technology client. This application aimed to facilitate communication among students and educators while also providing essential features for organizing classes and assessments.
+               </p>
+       
+            <hr />
+            <p className="card-text">
+              <strong>Key Functionality Developed:</strong>
+              <ul>
+                <li>Public and Private Chats: Implemented features enabling users to engage in both public and private chats, fostering communication and collaboration within the educational community.</li>
+                <li>Paid Class Management: Developed functionalities to organize paid classes, including scheduling, enrollment management, and integration with payment processing systems.</li>
+                <li>Class and Weekly Tests: Designed modules for conducting regular class tests and weekly assessments, enabling educators to evaluate student progress effectively.</li>
+                <li>Notifications: Integrated a notification system to alert users about upcoming classes, test schedules, and other important updates, ensuring timely communication.</li>
+                <li>Student Reports: Developed comprehensive reporting features to generate examination reports for students, providing insights into their performance and areas for improvement.</li>
+              </ul>
+
+              <p>
+              <strong>Used Skills:</strong> 
+
+ Python,Django Rest Framework,React.js,Bootstrap,CSS,PostgreSQL  </p>
+
+           
+            </p>
+                </div>
+        </div>
       </Container>
     </Container>
   );
